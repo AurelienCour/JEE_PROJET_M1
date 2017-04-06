948f354c6cb456fec9db41b8efaf4e49c8226e2d
Aurelien <Aurelien@DESKTOP-NHB7A0D>
2017-04-06 09:59:24 +0200

    Creation test

--- JEE_PROJET/src/java/PackageTest/TestAPI.java
+++ JEE_PROJET/src/java/PackageTest/TestAPI.java
@@ -5,10 +5,50 @@
  */
 package PackageTest;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  *
  * @author Aurelien
  */
 public class TestAPI {
+    
+    private API api;
+    
+    @Before
+    public void setUp()
+    {
+        api = new API();
+    }
+    
+    /**
+     * S'execute apres les test (pas utile ici)
+     */
+    @After
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void testInitialisation(){
+        api.Initialisation();
+        assertEquals("joueur1",api.statut());
+    }
+    
+    @Test
+    public void testTableau(){
+        for (Object object : api.getTab()) {
+            assertEquals(object.getAction(),api.action(object.getJoueur(),object.getCoup()));
+        }
+    }
+    
+    @Test
+    public void testDerniereAction(){
+        if(api.action(x,y)){
+            assertEquals(action.getY(),api.derniereAction());
+        }
+    }
     
 }
