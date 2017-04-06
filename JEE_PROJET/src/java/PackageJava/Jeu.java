740a30634f22a09a5baa55c5b4c6930dc7dfe901
Aurelien <Aurelien@DESKTOP-NHB7A0D>
2017-04-06 10:49:40 +0200

    Creation test

--- JEE_PROJET/src/java/PackageJava/Jeu.java
+++ JEE_PROJET/src/java/PackageJava/Jeu.java
@@ -9,6 +9,36 @@ package PackageJava;
  *
  * @author Aurelien
  */
-public class Jeu {
+public class Jeu extends API{
+
+    @Override
+    void initialisation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    boolean action(int numJoueur, int coup) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    String statut() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    int vainqueur() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    int derniereAction() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    String affichage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
