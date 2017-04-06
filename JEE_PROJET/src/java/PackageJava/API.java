7b41cc09bff7e105a5027dc81c7b2befe0b35e1a
Paul <Paul@DESKTOP-1U9DTNA>
2017-04-06 10:45:47 +0200

    abstract class API

--- JEE_PROJET/src/java/PackageJava/API.java
+++ JEE_PROJET/src/java/PackageJava/API.java
@@ -5,7 +5,7 @@ package PackageJava;
  * Classe qui permet d'utiliser les fonctions de l'application
  * @author Paul, Aurélien, Jonathan, Delphine, Claire
  */
-abstract class API {
+public abstract class API {
     
     private String statut;
     private int vainqueur;
@@ -37,11 +37,7 @@ abstract class API {
      * Function Initialisation
      * Permet d'initialiser les valeurs statuts, vainqueur et la dernière action
      */
-    public void initialisation(){
-        statut = "joueur1";
-        vainqueur = 0;
-        dernActionValide = 0;
-    }
+    abstract void initialisation();
     
     /**
      * Function action
@@ -50,45 +46,34 @@ abstract class API {
      * @param coup : coup efectué par le joueur
      * @return valide
      */
-    public boolean action(int numJoueur, int coup){
-        boolean valide = false;
-        return valide;
-    }
+    abstract boolean action(int numJoueur, int coup);
     
     /**
      * Function statut
      * Permet de renvoyer le joueur qui est en train de jouer ou si la partie est terminée
      * @return statut : "joueur1", "joueur2" ou "finie"
      */
-    public String statut(){
-        return statut;
-    }
+    abstract String statut();
     
     /**
      * Function vainqueur
      * Permet de retourner le numéro du joueur qui à gagné ou 0 si il y a match nul
      * @return vainqueur : 0 si match nul, 1 si le vainqueur et le joueur 1 et 2 si le joueur 2
      */
-    public int vainqueur(){
-        return vainqueur;
-    }
+    abstract int vainqueur();
     
     /**
      * Function derniereAction
      * Permet de renvoyer la dernière action qui a été effectué par le joueur
      * @return dernActionValide : numéro du coup
      */
-    public int derniereAction(){
-        return dernActionValide;
-    }
+    abstract int derniereAction();
     
     /**
      * Function affichage
      * Permet d'afficher ce qui c'est passé pendant l'action du joueur
      * @return action : action effectué par le joueur (coup effectué)
      */
-    public String affichage(){
-        return action;
-    }
+    abstract String affichage();
     
 }
